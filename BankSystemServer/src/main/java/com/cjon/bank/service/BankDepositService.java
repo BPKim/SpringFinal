3f3dc889f4c802eeb1c6fa2076a52d18a67e326f
BPKim <kbp0213@gmail.com>
2016-11-11 19:17:06 +0900

    aaaa

---
+++ BankSystemServer/src/main/java/com/cjon/bank/service/BankDepositService.java
@@ -0,0 +1,50 @@
+package com.cjon.bank.service;
+
+import java.sql.Connection;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.sql.DataSource;
+
+import org.springframework.ui.Model;
+
+import com.cjon.bank.dao.BankDAO;
+
+public class BankDepositService implements BankService {
+
+	@Override
+	public void execute(Model model) {
+		HttpServletRequest request = (HttpServletRequest) model.asMap().get("request");
+		String memberID = request.getParameter("memberId");
+		String memberBalance = request.getParameter("memberBalance");
+		DataSource dataSource = (DataSource) model.asMap().get("dataSource");
+		Connection con = null;
+
+		try {
+			con = dataSource.getConnection();
+			con.setAutoCommit(false);
+
+			BankDAO dao = new BankDAO(con);
+			boolean result = dao.updateDeposit(memberID, memberBalance);
+
+			dao.depositHistory(memberID, memberBalance);
+			
+			if (result)
+				con.commit();
+			else
+				con.rollback();
+
+			model.addAttribute("RESULT", result);
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				con.close();
+			} catch (Exception e) {		
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
